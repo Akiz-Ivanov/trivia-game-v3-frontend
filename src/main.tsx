713ee3dfcdf1006a1fa029ebf0f32efdbc1764df
Akiz-Ivanov <ivanovicandreja@gmail.com>
2025-09-16 23:10:11 +0200

    Fix API URLs by setting axios baseURL

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import './index.css'
 import App from './App.tsx'
 import { SettingsProvider } from './context/SettingsProvider.tsx'
 import { AuthProvider } from './context/AuthProvider.tsx'
+import axios from 'axios';
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
